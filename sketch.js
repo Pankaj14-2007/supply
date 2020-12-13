8ec4b3e208e003ef353b5d6c7c8a25398f93640b
Pankaj14-2007 <69744590+Pankaj14-2007@users.noreply.github.com>
2020-12-13 09:52:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+ 
+
 
 function preload()
 {
@@ -33,20 +35,15 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	var package_option ={
-		friction:1.0,
-		restitution:3,
-		 isSattic:false
-	}
 	
-	packageBody = Bodies.circle(width/2 , 200 , 5 ,package_option );
+	
+	packageBody = Bodies.circle(width/2 , 200 , 5, {restitution:0.4,isStatic:true} );
 	World.add(world, packageBody);
-    Matter.Body.setVelocity( packageBody, {x: 0, y: 0})
-	packageSprite.velocityY=0;
+  
 	console.log(packageBody);
 	
 	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	ground = Bodies.rectangle(width/2, 650, width, 10 ,{isStatic:true})
  	World.add(world, ground);
 
 
@@ -60,15 +57,14 @@ function draw() {
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
-  packageBody.velocityY=0;
+ 
   drawSprites();
  
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-	Matter.Body.setVelocity(packageBody, {x: 3, y: -3})
-  
+	Matter.Body.setStatic(packageBody,false)
   }
 }
 
